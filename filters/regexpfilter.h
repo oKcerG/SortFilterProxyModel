fcdc75d509298f7678d12df0ceb75674405e8954
r-vitanov <r.vitanov@bretgeld-engineering.de>
2021-11-12 11:21:23 +0100

    Removed pattern syntax and allowed for filtering and sorting only on specific roles

--- filters/regexpfilter.h
+++ filters/regexpfilter.h
@@ -7,47 +7,34 @@
 namespace qqsfpm {
 
 class RegExpFilter : public RoleFilter {
-    Q_OBJECT
-    Q_PROPERTY(QString pattern READ pattern WRITE setPattern NOTIFY patternChanged)
-    Q_PROPERTY(PatternSyntax syntax READ syntax WRITE setSyntax NOTIFY syntaxChanged)
-    Q_PROPERTY(Qt::CaseSensitivity caseSensitivity READ caseSensitivity WRITE setCaseSensitivity NOTIFY caseSensitivityChanged)
+  Q_OBJECT
+  Q_PROPERTY(
+      QString pattern READ pattern WRITE setPattern NOTIFY patternChanged)
+  Q_PROPERTY(Qt::CaseSensitivity caseSensitivity READ caseSensitivity WRITE
+                 setCaseSensitivity NOTIFY caseSensitivityChanged)
 
 public:
-    enum PatternSyntax {
-        RegExp,// = QRegularExpression::RegExp,
-        Wildcard,// = QRegularExpression::Wildcard,
-        FixedString,// = QRegularExpression::FixedString,
-        RegExp2,// = QRegularExpression::RegExp2,
-        WildcardUnix,// = QRegularExpression::WildcardUnix,
-        W3CXmlSchema11,/* = QRegularExpression::W3CXmlSchema11*/ };
-    Q_ENUMS(PatternSyntax)
+  using RoleFilter::RoleFilter;
 
-    using RoleFilter::RoleFilter;
+  QString pattern() const;
+  void setPattern(const QString &pattern);
 
-    QString pattern() const;
-    void setPattern(const QString& pattern);
-
-    PatternSyntax syntax() const;
-    void setSyntax(PatternSyntax syntax);
-
-    Qt::CaseSensitivity caseSensitivity() const;
-    void setCaseSensitivity(Qt::CaseSensitivity caseSensitivity);
+  Qt::CaseSensitivity caseSensitivity() const;
+  void setCaseSensitivity(Qt::CaseSensitivity caseSensitivity);
 
 protected:
-    bool filterRow(const QModelIndex& sourceIndex, const QQmlSortFilterProxyModel& proxyModel) const override;
+  bool filterRow(const QModelIndex &sourceIndex,
+                 const QQmlSortFilterProxyModel &proxyModel) const override;
 
 Q_SIGNALS:
-    void patternChanged();
-    void syntaxChanged();
-    void caseSensitivityChanged();
+  void patternChanged();
+  void caseSensitivityChanged();
 
 private:
-    QRegularExpression m_regExp;
-    Qt::CaseSensitivity m_caseSensitivity = m_regExp.caseSensitivity();
-    PatternSyntax m_syntax = static_cast<PatternSyntax>(m_regExp.patternSyntax());
-    QString m_pattern = m_regExp.pattern();
+  QRegularExpression m_regularExpression;
+  QString m_pattern{m_regularExpression.pattern()};
 };
 
-}
+} // namespace qqsfpm
 
 #endif // REGEXPFILTER_H
